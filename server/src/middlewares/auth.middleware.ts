9602afbfde97af87cfc9a9117ee9cd7622fb76fb
Vladyslav Geyna <vladgeina@gmail.com>
2023-08-18 13:47:59 +0300

    Created auth middleware

---
+++ server/src/middlewares/auth.middleware.ts
@@ -0,0 +1,30 @@
+import tokenService from '@resources/token/token.service'
+import HttpError from '@utils/exceptions/http.error'
+import { NextFunction, Request, Response } from 'express'
+
+const authMiddleware = (req: Request, res: Response, next: NextFunction) => {
+	try {
+		const authHeader = req.headers.authorization
+		if (!authHeader) {
+			return next(HttpError.UnauthorizedError())
+		}
+
+		const accessToken = authHeader.split(' ')[1]
+		if (!accessToken) {
+			return next(HttpError.UnauthorizedError())
+		}
+
+		const userData = tokenService.validateAccessToken(accessToken)
+		if (!userData) {
+			return next(HttpError.UnauthorizedError())
+		}
+
+		// req.user = userData
+
+		next()
+	} catch (error) {
+		return next(HttpError.UnauthorizedError())
+	}
+}
+
+export default authMiddleware
